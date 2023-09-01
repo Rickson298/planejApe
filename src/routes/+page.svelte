04c6bcb3bb5f8e27c05be513a926e9b1cec37a18
Rickson Oliveira <88171582+Rickson298@users.noreply.github.com>
2023-08-31 23:47:57 -0300

    feat: - Eslint config - Phosphor icons lib - CSS reset - Font poppins

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+	<title>Página inicial</title>
+	<meta name="description" content="Organize seu apartamento facilmente com o planejApê" />
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<section>
+	<h1>Hello world</h1>
+</section>
