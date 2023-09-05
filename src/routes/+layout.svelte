8856d95fb1587262fb1a0374ec914253ae86b68c
Rickson Oliveira <rickson.patrick13@gmail.com>
2023-09-05 00:26:32 -0300

    feat: page transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,19 @@
-<script>
+<script lang="ts">
+	import PageTransition from '$components/PageTransition.svelte';
 	import { Sidebar } from '$core/components';
+	import type { LayoutServerData } from './$types';
 	import './styles.css';
+
+	export let data: LayoutServerData;
 </script>
 
 <div class="app">
 	<main>
 		<Sidebar />
 
-		<slot />
+		<PageTransition pathname={data.pathname}>
+			<slot />
+		</PageTransition>
 	</main>
 </div>
 
--- src/routes/produtos/+page.svelte
+++ src/routes/produtos/+page.svelte
@@ -1,1 +1,6 @@
+<svelte:head>
+	<title>Produtos</title>
+	<meta name="description" content="Organize seu apartamento facilmente com o planejApê" />
+</svelte:head>
+
 <div>Página de produtos</div>
