d14e4a91afbbb93c7993343ad134cc1896221faa
Rickson Oliveira <rickson.patrick13@gmail.com>
2023-09-07 22:28:07 -0300

    feat: popup user menu

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 import Flex from './flex/Flex.svelte';
 import MenuHamburguer from './menu-hamburguer/MenuHamburguer.svelte';
 import Typography from './typography/Typography.svelte';
-import Avatar from './avatar/Avatar.svelte';
 
-export { Flex, MenuHamburguer, Typography, Avatar };
+export { Flex, MenuHamburguer, Typography };
--- src/core/components/header/Header.svelte
+++ src/core/components/header/Header.svelte
@@ -1,16 +1,57 @@
-<script>
-	import { Avatar } from '$components';
+<script lang="ts">
+	import { fly } from 'svelte/transition';
+	import Gear from 'phosphor-svelte/lib/Gear';
+	import SignOut from 'phosphor-svelte/lib/SignOut';
+	import UserSwitch from 'phosphor-svelte/lib/UserSwitch';
+	import { Typography } from '$components';
+
+	let isOpenPopup = false;
+	let containerMenu: HTMLButtonElement | null;
+
+	function handleUserOptions() {
+		isOpenPopup = !isOpenPopup;
+	}
+
+	function closeUserOptions() {
+		isOpenPopup = false;
+	}
+
+	$: if (!isOpenPopup) {
+		containerMenu?.blur();
+	}
 </script>
 
-<header><Avatar fullName="Rickson Oliveira" /></header>
+<header>
+	<button
+		bind:this={containerMenu}
+		class="container_menu_options"
+		on:click={handleUserOptions}
+		on:blur={closeUserOptions}
+	>
+		{#if isOpenPopup}
+			<div class="popup" transition:fly={{ y: 25 }}>
+				<button class="popup_option">
+					<UserSwitch size={24} color="var(--blue-300)" />
+					<Typography fontWeight="400">Trocar de conta</Typography>
+				</button>
+				<button class="popup_option">
+					<SignOut size={24} color="var(--blue-300)" />
+					<Typography fontWeight="400">Sair</Typography>
+				</button>
+			</div>
+		{/if}
 
-<style lang="scss">
-	header {
-		width: 100%;
-		padding-right: 1rem;
-		display: flex;
-		justify-content: end;
-		align-items: center;
-		height: 80px;
-	}
+		<Typography fontWeight="400">Rickson Oliveira</Typography>
+		<div class="avatar">
+			<div class="avatar_content">
+				<span class="username">RO</span>
+				<div class="container_icon">
+					<Gear class="teste" size={28} />
+				</div>
+			</div>
+		</div>
+	</button>
+</header>
+
+<style src="./styles.scss">
 </style>
