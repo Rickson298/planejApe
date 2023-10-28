24ab9b7a510626356cf921d221c74f1f38d30b7c
Rickson Oliveira <rickson.patrick13@gmail.com>
2023-10-28 00:11:23 -0300

    fix: [Layout] - remove Breadcrumb

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,34 +1,11 @@
 <script lang="ts">
-	import { tick } from 'svelte';
-	import { Breadcrumb } from 'planejape-ui-svelte';
-	import House from 'phosphor-svelte/lib/House';
-
 	import { Header, PageTransition, Sidebar } from '$core/components';
 
 	import './styles.css';
 
-	import type { BreadcrumbItem } from 'planejape-ui-svelte';
 	import type { LayoutServerData } from './$types';
 
-	import { PAGE_ROUTES } from '$shared/constants/page-routes';
-
 	export let data: LayoutServerData;
-
-	$: currentPath = PAGE_ROUTES.find((route) => route.path === data.pathname);
-
-	const homeBreadcrumb: BreadcrumbItem = { label: 'Início', href: '/', icon: House };
-
-	let breadcrumbs: BreadcrumbItem[] = [];
-
-	$: {
-		if (currentPath) {
-			breadcrumbs = [homeBreadcrumb, { href: currentPath.path, label: currentPath.label }];
-		} else {
-			breadcrumbs = [homeBreadcrumb];
-		}
-
-		tick();
-	}
 </script>
 
 <main>
@@ -37,9 +14,6 @@
 		<Header />
 
 		<PageTransition pathname={data.pathname}>
-			{#if data.pathname !== '/'}
-				<Breadcrumb items={breadcrumbs} />
-			{/if}
 			<slot />
 		</PageTransition>
 	</div>
