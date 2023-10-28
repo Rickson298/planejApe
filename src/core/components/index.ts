7c191753d5020ef3935f86ac054b495edd17930d
Rickson Oliveira <rickson.patrick13@gmail.com>
2023-10-28 00:04:25 -0300

    fix: export components in index

--- src/core/components/index.ts
+++ src/core/components/index.ts
@@ -1,5 +1,7 @@
-import Sidebar from './sidebar/Sidebar.svelte';
-import PageTransition from './page-transition/PageTransition.svelte';
-import Header from './header/Header.svelte';
-
-export { Sidebar, PageTransition, Header };
+export { default as Sidebar } from './sidebar/Sidebar.svelte';
+export { default as PageTransition } from './page-transition/PageTransition.svelte';
+export { default as Header } from './header/Header.svelte';
+export { default as DesktopContainer } from './desktop-container/DesktopContainer.svelte';
+export { default as LaptopContainer } from './laptop-container/LaptopContainer.svelte';
+export { default as TabletContainer } from './tablet-container/TabletContainer.svelte';
+export { default as MobileContainer } from './mobile-container/MobileContainer.svelte';
